da14bd9fe2627ba32ff8a7547d4f8bbb8c6186d0
Enzo949494 <9812@holbertonstudents.com>
2025-01-22 10:58:17 +0100

    try to fix again the 1st task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    output = ""
-    try:
-        for i in range(x):
-            output += str(my_list[i])
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
             printed += 1
-    except:
-        pass
-    finally:
-        print(output)
+        except Exception:
+            break
+    print("")
     return printed
