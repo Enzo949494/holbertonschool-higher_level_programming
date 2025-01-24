707d72170cc305f13c93d0cd897cd7e5ff0f751d
Enzo949494 <9812@holbertonstudents.com>
2025-01-24 11:30:25 +0100

    fix pystyle blankline

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,17 +3,18 @@
 Module with a function for adding two integers
 """
 
+
 def add_integer(a, b=98):
     """
     Adds 2 integers and returns the result
-    
+
     Args:
         a (int or float): The first number
         b (int or float, optional): The second number. Defaults to 98
-    
+
     Returns:
         int: The sum of a and b
-    
+
     Raises:
         TypeError: If a or b is not an integer or float.
     """
