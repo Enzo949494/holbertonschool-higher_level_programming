9468b24d08907bbc389d052b183515d98e43db60
Enzo949494 <9812@holbertonstudents.com>
2025-01-25 11:53:52 +0100

    prints a text with 2 new lines after each of these characters:(., ? and :)

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+    text (str): The input text to be formatted.
+
+    Raises:
+    TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    punctuation = ['.', '?', ':']
+    start = 0
+    for i, char in enumerate(text):
+        if char in punctuation:
+            print(text[start:i+1].strip() + "\n")
+            start = i + 1
+
+    if start < len(text):
+        print(text[start:].strip(), end="")
