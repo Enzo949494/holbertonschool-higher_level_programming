bfed173a0f3847e66ce7c4f1ed026d4e8528e658
Enzo949494 <9812@holbertonstudents.com>
2025-01-27 11:13:08 +0100

    1st and 2nd task

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""0-square.py"""
+
+
+class Square:
+	"""empty class Square that defines a square"""
+	pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+class Square:
+    """
+    A class that defines a square.
+    
+    Attributes:
+        __size (int): The size of the square's side.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the Square instance with a given size.
+
+        Args:
+            size (int): The size of the square's side.
+        """
+        self.__size = size
