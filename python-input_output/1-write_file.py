a1f2d81f39c2ee7d9468c26158fb35a82959f0f1
Enzo949494 <9812@holbertonstudents.com>
2025-02-10 12:18:52 +0100

    fix pystyle for task1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Create a function that writes text to a file and 
+"""Create a function that writes text to a file and
     reports how many characters it has written"""
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    """Writes a string to a text file (UTF8)
+    and returns the number of characters written."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
