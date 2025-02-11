14f103d01207e35ff3ed7471d00a9fe1f42a487e
Enzo949494 <9812@holbertonstudents.com>
2025-02-11 08:35:58 +0100

    fix style for pascal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -8,17 +8,16 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Start with the first row of Pascal's triangle
+    triangle = [[1]]
 
     for i in range(1, n):
-        prev_row = triangle[-1]  # Get the previous row
-        new_row = [1]  # Every row starts with 1
+        prev_row = triangle[-1]
+        new_row = [1]
 
-        # Calculate the values in between
         for j in range(1, len(prev_row)):
             new_row.append(prev_row[j - 1] + prev_row[j])
 
-        new_row.append(1)  # Every row ends with 1
-        triangle.append(new_row)  # Add the new row to the triangle
+        new_row.append(1)
+        triangle.append(new_row)
 
     return triangle
