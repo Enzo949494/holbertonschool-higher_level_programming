38cd7c504eeb0147e76d7f856105472c24714a7d
Enzo949494 <9812@holbertonstudents.com>
2025-03-04 14:29:54 +0000

    fuck this part2

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,35 +1,9 @@
 -- list all city of california in DB
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- use
-USE hbtn_0d_usa;
-
--- create 
-CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-);
-
--- create
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
-);
-
--- insert
-INSERT INTO states (name) VALUES ('California'), ('Arizona'), ('Texas'), ('Utah');
-
--- insert
-INSERT INTO cities (state_id, name) VALUES (1, 'San Francisco'), (1, 'San Jose'), (2, 'Page'), (3, 'Paris'), (3, 'Houston'), (3, 'Dallas');
-
--- request for list
-SELECT id, name
+SELECT cities.id, cities.name
 FROM cities
-WHERE state_id = (
+WHERE cities.state_id = (
     SELECT id
     FROM states
     WHERE name = 'California'
 )
-ORDER BY id ASC;
+ORDER BY cities.id ASC;
