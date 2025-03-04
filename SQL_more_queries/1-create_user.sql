ca898a9b0ff046b4bbabab347aad6f98379c3522
Enzo949494 <9812@holbertonstudents.com>
2025-03-04 13:06:44 +0000

    create,give and apply

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- create user_0d_1 if doesnt exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- give all grants to user1 on all database and tables
+GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- apply settings immediatly
+FLUSH PRIVILEGES;
+
