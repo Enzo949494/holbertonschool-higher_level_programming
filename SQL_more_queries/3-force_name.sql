9c8a1f5b8f8d3d1dda56de2be288fcdcb6dccf96
Enzo949494 <9812@holbertonstudents.com>
2025-03-04 13:36:47 +0000

    create table,row(int),row(varchar)notnull/2nd try,fix syntax

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,2 @@
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
-);
+-- create table and row varchar
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
