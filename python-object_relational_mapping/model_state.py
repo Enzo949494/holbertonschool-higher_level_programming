dbf069bc96628710147ef19d0d6b6024a69b203e
Enzo949494 <9812@holbertonstudents.com>
2025-03-06 15:59:59 +0100

    add modif in basemodel for task 14

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@ Defines a State class and an instance of Base for SQLAlchemy ORM.
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
+from model_state import Base
 # Create the Base class for declarative mapping
 Base = declarative_base()
 
@@ -22,3 +22,18 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+class City(Base):
+    """
+    City class that links to the MySQL table 'cities'.
+    Attributes:
+        id (int): The city's id, primary key, auto-generated, unique, not null.
+        name (str): The city's name, string with max 128 characters, not null.
+        state_id (int): The state's id, foreign key to states.id, not null.
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
