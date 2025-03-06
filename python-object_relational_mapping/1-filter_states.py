b887b7925b7f5b62cf2dc816bc74ad0282b9a6e2
Enzo949494 <9812@holbertonstudents.com>
2025-03-06 12:47:30 +0000

    task1,list states N,5 try

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,12 @@ from the database hbtn_0e_0_usa.
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
+    """
+    Connects to the database, retrieves and displays all states
+    with a name starting with 'N' in ascending order by states.id.
+    """
     # Connect to the MySQL server
     db = MySQLdb.connect(
         host="localhost",
