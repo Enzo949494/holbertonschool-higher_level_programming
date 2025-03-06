c82cdc394ad6b3eede8bbdbefbe18b5ab2e07e03
Enzo949494 <9812@holbertonstudents.com>
2025-03-06 12:44:09 +0000

    task1,list states N,4 try

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,26 +8,20 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
     # Connect to the MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     # Create a cursor object to execute queries
     cursor = db.cursor()
 
     # Execute the SQL query to retrieve states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
-    cursor.execute(query)
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
     # Fetch all rows from the executed query
     rows = cursor.fetchall()
